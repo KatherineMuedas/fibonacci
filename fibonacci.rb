70e16bce654a91606a993bf91d77e285b7ca0ba8
katherine <muedasnka@gmail.com>
2019-03-15 15:14:11 -0400

    adds fibonacci test zero is zero, and make it pass

---
+++ fibonacci.rb
@@ -0,0 +1,5 @@
+class Fibonacci
+  def self.of(number)
+    0
+  end
+end
---
+++ test_fibonacci.rb
@@ -0,0 +1,9 @@
+require 'minitest/autorun'
+require_relative 'fibonacci'
+
+class TestFibonacci < Minitest::Test
+  def test_fibonacci_zero_is_zero
+      result = Fibonacci.of(0)
+      assert_equal 0, result
+  end
+end
